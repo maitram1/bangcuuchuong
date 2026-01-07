43ff5ae27fae6d78c4c4f934f9c63d6200329417
Mai TRâm <lethimaitram432@gmail.com>
2026-01-07 20:47:36 +0800

    Add files via upload

---
+++ my.js
@@ -0,0 +1,11 @@
+document.write("<table border ='1'>")
+for(let i=1;i<10;i++){
+    document.write("<tr>");
+    for(let j=1;j<10;j++){
+        document.write("<td>");
+        document.write(i+"x"+j+"="+i*j+" ");
+        document.write("</td>")
+    }
+    document.write("</tr>");
+}
+document.write("</table>")
